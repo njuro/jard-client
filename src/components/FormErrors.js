e9d00cdfc3ef23e6723acba8380840888abc51bc
njuro <juraj.noge@gmail.com>
2019-11-18 23:09:51 +0100

    Handle null in FormErrors

--- src/components/FormErrors.js
+++ src/components/FormErrors.js
@@ -4,7 +4,7 @@ import {List, Message} from 'semantic-ui-react';
 function FormErrors({errors}) {
     return (
         errors && errors.length > 0 && <Message error content={<List items={errors} bulleted/>}/>
-    );
+    ) || null;
 }
 
 export default FormErrors;
