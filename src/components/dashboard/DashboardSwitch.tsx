88b87bd41e9ec7b824dfeff2fbc75599cb9ed6c4
njuro <juraj.noge@gmail.com>
2020-06-30 10:58:42 +0200

    Logged user nick on dashboard

--- src/components/dashboard/DashboardSwitch.tsx
+++ src/components/dashboard/DashboardSwitch.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Route, Switch } from "react-router-dom";
 import { DASHBOARD_URL } from "../../helpers/mappings";
 import BoardAdmin from "../board/BoardAdmin";
 import UserAdmin from "../user/UserAdmin";
 import BanAdmin from "../ban/BanAdmin";
+import { AuthContext } from "../App";
 
 function DashboardSwitch() {
+  const { user } = useContext(AuthContext);
   return (
     <Switch>
       <Route
@@ -19,7 +21,13 @@ function DashboardSwitch() {
         component={UserAdmin}
       />
       <Route exact path={`${DASHBOARD_URL}/manage-bans`} component={BanAdmin} />
-      <Route render={() => <div>Hello dashboard</div>} />
+      <Route
+        render={() => (
+          <div>
+            Welcome to dashboard, <strong>{user.username}</strong>
+          </div>
+        )}
+      />
     </Switch>
   );
 }
