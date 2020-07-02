956d16d502cbeef2e8e79bd47af2c713d00a03a3
njuro <juraj.noge@gmail.com>
2020-07-02 09:01:21 +0200

    React API URL from enviroment variable

--- src/helpers/api.ts
+++ src/helpers/api.ts
@@ -1,7 +1,8 @@
 import axios, { Method } from "axios";
 
+const baseURL = process.env.REACT_APP_API_URL;
 const client = axios.create({
-  baseURL: `http://localhost:8081/api/`,
+  baseURL: baseURL || `http://localhost:8081/api/`,
   withCredentials: true,
 });
 
