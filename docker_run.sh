42b92427a2878d7380adad6cdf551703cbe97786
njuro <juraj.noge@gmail.com>
2020-08-08 14:19:28 +0200

    Set restart policy for docker container

--- docker_run.sh
+++ docker_run.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # script for running jard client as docker container
 # don't forget to set enviroment variables in .env file first
-docker run -itd --network=host --env-file .env --name jard-client njuro/jard-client:latest
+docker run -itd --network=host --restart unless-stopped --env-file .env --name jard-client njuro/jard-client:latest
