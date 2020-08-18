b1795f69ec0e278dfdff343707c2696494a4404c
njuro <juraj.noge@gmail.com>
2020-08-18 08:19:10 +0200

    Disable client validation on CAPTCHA

--- src/components/form/Captcha.tsx
+++ src/components/form/Captcha.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import HCaptcha from "@hcaptcha/react-hcaptcha";
-import { Controller, useFormContext } from "react-hook-form";
+import { Controller } from "react-hook-form";
 import { isLocalhost } from "../../helpers/utils";
 
 interface CaptchaProps {
@@ -8,8 +8,7 @@ interface CaptchaProps {
 }
 function Captcha({ name }: CaptchaProps) {
   const DUMMY_SITE_KEY = "10000000-ffff-ffff-ffff-000000000001";
-
-  const { setError, clearErrors } = useFormContext();
+  const SITE_KEY = process.env.REACT_APP_HCAPTCHA_SITE_KEY || DUMMY_SITE_KEY;
 
   if (isLocalhost()) {
     return (
@@ -25,27 +24,10 @@ function Captcha({ name }: CaptchaProps) {
       name={name}
       render={({ onChange }) => (
         <HCaptcha
-          sitekey={process.env.REACT_APP_HCAPTCHA_SITE_KEY ?? DUMMY_SITE_KEY}
+          sitekey={SITE_KEY}
           size="normal"
           theme="dark"
-          onVerify={(token) => {
-            onChange(token);
-            clearErrors(name);
-          }}
-          onError={(error) => {
-            setError(name, {
-              type: "validate",
-              message: `Captcha returned error: ${error}`,
-            });
-            onChange(undefined);
-          }}
-          onExpire={() => {
-            setError(name, {
-              type: "validate",
-              message: "Captcha has expired",
-            });
-            onChange(undefined);
-          }}
+          onVerify={(token) => onChange(token)}
         />
       )}
     />
